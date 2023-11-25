c75ab841d6e6ee84b6a29d3be2d8b20c0911449f
Shariful <sajukhan12905@gmail.com>
2023-11-26 00:05:24 +0600

    learning React

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './newCss.css'
+const name = 'Shariful Islam'
+const date = new Date();
+const dayName = date.getDate();
+const monthName = date.getMonth();
+const yearName = date.getFullYear();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h2>Hello</h2>
+    <p>{name}</p>
+    <p>Today's date {dayName + '/' + (monthName + 1) + '/' + yearName}</p>
+    <p className='green'>Background Green</p>
+  </div>
+
+
+
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
